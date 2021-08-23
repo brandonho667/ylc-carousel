ff6a82afee4a73fc4a7db3f1cf8026a7ffb85939
Brandon <b1ho@ucsd.edu>
2021-08-22 23:06:48 -0700

    first commit

---
+++ carousel.js
@@ -0,0 +1,24 @@
+jQuery(function($) {
+	function carousel_show_another_link(direction) {
+        var ul = $('#carousel ul');
+        var current = -parseInt(ul[0].style.marginLeft) / 100;
+        var new_link = current + direction;
+        var links_number = ul.children('li').length;
+        if (new_link >= 0 && new_link < links_number) {
+            ul.animate({'margin-left': -(new_link * 100) + '%'});
+        }
+    }
+    function carousel_previous_link() {
+        carousel_show_another_link(-1);
+        return false;
+    }
+    function carousel_next_link() {
+        carousel_show_another_link(1);
+        return false;
+    }
+    $(function() {
+		$('#carousel ul li a.carousel-prev').click(carousel_previous_link);
+        $('#carousel ul').css('margin-left', 0);
+        $('#carousel ul li a.carousel-next').click(carousel_next_link);
+	});
+});
---
+++ ylc-carousel.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Plugin Name: YLC Carousel
+ * Description: Carousel for the website YLC
+ * Author: Brandon Ho
+ * Version: 1.0.0
+ */
+
+function console_log($output, $with_script_tags = true) {
+    $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) . 
+');';
+    if ($with_script_tags) {
+        $js_code = '<script>' . $js_code . '</script>';
+    }
+    echo $js_code;
+}
+
+function display_carousel() {
+    console_log("cock and balls");
+    $args = array(
+        'category_name' => 'carousel',
+        'orderby' => 'link_id',
+        'order' => 'DESC',
+        'limit' => 5
+    );
+    
+    $links = get_bookmarks($args);
+    $n = count($links);
+
+    if (!empty($links)) {
+        wp_register_script('carousel-js', plugin_dir_url(__FILE__) . 'carousel.js', array('jquery'), '1.0', true);
+        wp_enqueue_script('carousel-js');
+        ?>
+        <div id="carousel">
+            <ul style="width: <?php echo $n * 100; ?>%;">
+            <?php
+            foreach ($links as $i => $link) {
+                // Background image
+                if (!empty($link->link_image))
+                    $background = 'url(' . $link->link_image . ')';
+                else
+                    $background = 'rgb(' . rand(0, 255) . ', ' . rand(0, 255) . ', ' . rand(0, 255) . ')';
+                // Target attribute
+                if (!empty($link->link_target))
+                    $target = ' target="' . $link->link_target . '"';
+                else
+                    $target = '';
+                // Rel attribute
+                if (!empty($link->link_rel))
+                    $rel = ' rel="' . $link->link_rel . '"';
+                else
+                    $rel = '';
+                ?>
+                <li style="background: <?php echo $background; ?>">
+                    <a href="<?php echo $link->link_url; ?>" class="carousel-link">
+                        <strong><?php echo $link->link_name; ?></strong>
+                        <?php
+                        if (!empty($link->link_description)) {
+                            ?>
+                            <em><?php echo $link->link_description; ?></em>
+                            <?php
+                        }
+                        ?>
+                    </a>
+                    <?php
+                    // Previous link
+                    if ($i > 0) {
+                        ?>
+                        <a href="#prev" class="carousel-prev">&lt;</a>
+                        <?php
+                    }
+                    ?>
+                    <?php
+                    // Next link
+                    if ($i < $n - 1) {
+                        ?>
+                        <a href="#next" class="carousel-next">&gt;</a>
+                        <?php
+                    }
+                    ?>
+                </li>
+                <?php
+            }
+            ?>
+            </ul>
+        </div>
+        <?php
+    }
+}
+
+function enqueue_carousel_style() {
+    wp_register_style('carousel-css', plugin_dir_url(__FILE__) . 'carousel.css');
+    wp_enqueue_style('carousel-css', plugin_dir_url(__FILE__) . 'carousel.css');
+}
+add_action('wp_enqueue_scripts', 'enqueue_carousel_style');
+add_shortcode('ylc-carousel', 'display_carousel');
+
+add_filter( 'pre_option_link_manager_enabled', '__return_true' );
